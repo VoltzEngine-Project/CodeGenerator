f95f4fd16442c7748d448a16df099064dd40277b
Robert Seifert <rseifert.phone@gmail.com>
2017-07-29 18:00:41 -0400

    Sync

---
+++ src/main/java/com/builtbroken/mc/codegen/item/ItemBattery.java
@@ -0,0 +1,11 @@
+package com.builtbroken.mc.codegen.item;
+
+/**
+ * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
+ * Created by Dark(DarkGuardsman, Robert) on 5/30/2017.
+ */
+public class ItemBattery
+{
+    //TODO create single use batteries that use redstone and glowstone as a fuel cell
+    //TODO create rechargable batteries with tier levels
+}
